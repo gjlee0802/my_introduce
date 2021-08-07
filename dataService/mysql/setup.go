171a19e0418e265589fb0e4a1ff08318dc752e65
gjlee0802 <gjlee0802@naver.com>
2021-08-07 17:30:34 +0900

    Checkpoint

--- dataService/mysql/setup.go
+++ dataService/mysql/setup.go
@@ -1,10 +1,28 @@
 package mysql
 
 import (
-	"github.com/gjlee0802/my_introduce/domain/model"
+	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"github.com/gjlee0802/my_introduce/domain/model"
 )
 
-var dbConn *gorm2.DB
+const (
+	dbms = "mysql"
+	user = "root"
+	pass = "password"
+	db = "my_introduce"
+)
 
+var dbConn *gorm.DB
+
+func Setup() {
+	var err error
+	conn := fmt.Sprintf("%s:%s@/%s?parseTime=true", user, pass, db)
+	dbConn, err = gorm.Open(dbms, conn)
+	if err != nil{
+		panic(err)
+	}
+
+	dbConn.AutoMigrate(&model.User{},)
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module my_introduce
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.3
+require (
+	github.com/gin-gonic/gin v1.7.3
+	gorm.io/driver/mysql v1.1.1
+	gorm.io/gorm v1.21.12
+)
